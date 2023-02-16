eef510fe1adbf83ca769be488853b70cdb80061c
Jeonkukil <a0211a@naver.com>
2023-02-17 04:00:06 +0900

    게시물 목록보기

--- src/main/java/shop/mtcoding/newblog/model/BoardRepository.java
+++ src/main/java/shop/mtcoding/newblog/model/BoardRepository.java
@@ -5,18 +5,22 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import shop.mtcoding.newblog.dto.board.BoardResp.BoardMainRespDto;
+
 @Mapper
 public interface BoardRepository {
 
-    public List<Board> findAll();
+        public List<Board> findAll();
 
-    public Board findById(int id);
+        public Board findById(int id);
 
-    public int insert(@Param("title") String title, @Param("content") String content,
-            @Param("userId") int userId);
+        public int insert(@Param("title") String title, @Param("content") String content,
+                        @Param("userId") int userId);
 
-    public int updateById(@Param("id") int id, @Param("title") String title,
-            @Param("content") String content);
+        public int updateById(@Param("id") int id, @Param("title") String title,
+                        @Param("content") String content);
 
-    public int deleteById(int id);
+        public int deleteById(int id);
+
+        public List<BoardMainRespDto> findAllWithUser();
 }
